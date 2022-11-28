600a58e41b59be0cbbed1f859d13e936f45b6bd2
Priyanshu <coolpriyanshu236@gmail.com>
2022-11-28 19:06:42 +0530

    Changed Api

--- popup.js
+++ popup.js
@@ -1,63 +1,63 @@
 
-let generateBtn=document.getElementById('short_btn')
-let s_url=document.getElementById("short_url")
-
-generateBtn.addEventListener('click', (e)=>{
-    if(!s_url.value)
-    {
-        document.getElementById('error-msg').style.display='block';
-        document.getElementById('short-link').style.display='none';
+let generateBtn = document.getElementById('short_btn')
+let s_url = document.getElementById("short_url")
+let icon=document.getElementById('clipIcon')
+icon.addEventListener('click',(e)=>{
+    console.log("hello")
+    navigator.clipboard.writeText(document.getElementById('short-text').innerHTML)
+    document.getElementById('clipboard').style.display = 'block'
+        setTimeout(() => {
+            document.getElementById('clipboard').style.display = 'none'
+            
+        }, (2000));
+})
+generateBtn.addEventListener('click', (e) => {
+    if (!s_url.value) {
+        document.getElementById('error-msg').style.display = 'flex';
+        document.getElementById('short-link').style.display = 'none';
+        
     }
-    else
-    {
-       
-            chrome.storage.sync.get('API_Token', function(result) {
-            if(!result.API_Token)
-            {
-                document.getElementById('error-msg').style.display='none'
-                document.getElementById('short-link').innerHTML="Provide API key in options from t.ly website"
-                document.getElementById('short-link').style.display='block'
+    else {
+
+        chrome.storage.sync.get('API_Token', function (result) {
+            if (!result.API_Token) {
+                document.getElementById('error-msg').style.display = 'none'
+                document.getElementById('short-text').innerHTML = "Provide API key in options from rebrandly website"
+                document.getElementById('short-link').style.display = 'flex'
                 return;
             }
-            const url = new URL(
-            "https://t.ly/api/v1/link/shorten"
-            );
-            
-            const params = {
-                "api_token": result.API_Token,
+            console.log(result.API_Token)
+            const options = {
+                method: 'POST',
+                headers: {
+                    accept: 'application/json',
+                    'content-type': 'application/json',
+                    apikey: result.API_Token
+                },
+                body: JSON.stringify({
+                    destination: s_url.value
+                })
             };
-            Object.keys(params)
-            .forEach(key => url.searchParams.append(key, params[key]));
-            
-            const headers = {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-            };
-        
-        let body = {
-            "long_url": s_url.value,
-            "domain": "https://t.ly/",
-            "include_qr_code": false
-        };
-        
-        fetch(url, {
-            method: "POST",
-            headers,
-            body: JSON.stringify(body),
-        }).then((response)=>response.json() ).then((json) =>{
-            console.log(json)
-            if(json.provider_success==false)
-            {
-                document.getElementById('error-msg').style.display='block'
-                document.getElementById('error-msg').innerHTML="Something wrong with your API Token"
-                document.getElementById('short-link').style.display='none';
-                return;
-            }
-            document.getElementById('error-msg').style.display='none'
-            document.getElementById('short-link').innerHTML=json.short_url
-            document.getElementById('short-link').style.display='block'
+            fetch('https://api.rebrandly.com/v1/links', options)
+                .then(response => response.json())
+                .catch( (err)=>{
+                    document.getElementById('error-msg').style.display = 'flex'
+                    document.getElementById('error-msg').innerHTML = "Something wrong with your API Token. Enter new api key from options"
+                    document.getElementById('short-link').style.display = 'none';
+                    console.error(err)
+                    
+                })
+                .then(response => {
+                    document.getElementById('error-msg').style.display = 'none'
+                    document.getElementById('short-text').innerHTML = response.shortUrl
+                    document.getElementById('short-link').style.display = 'flex'
+                    
+                    console.log(response)
+                })
+                
         });
-    });
+
+
     }
     e.preventDefault();
 })
